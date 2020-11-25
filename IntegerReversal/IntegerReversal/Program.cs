514bd4942cb63951815adf09a3b538882f0cf52e
Mohammad Sahebhonar <msahebhonar@yahoo.com>
2020-11-26 01:12:05 +0330

    Integer Reversal
    Given an integer, reverse digits of the Integer!

---
+++ IntegerReversal/IntegerReversal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace IntegerReversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var st = new Stopwatch();
+
+            st.Start();
+            Console.WriteLine(ReverseInteger(short.MaxValue));
+            Console.WriteLine($"Time Elapsed: {st.Elapsed}");
+
+            st.Reset();
+            st.Start();
+            Console.WriteLine(ReverseIntegerV2(short.MaxValue));
+            Console.WriteLine($"Time Elapsed: {st.Elapsed}");
+        }
+
+        public static int ReverseInteger(int number)
+        {
+            var str = Math.Abs(number).ToString().ToCharArray();
+            Array.Reverse(str);
+            return int.Parse(str) * Math.Sign(number);
+        }
+
+        public static int ReverseIntegerV2(int number)
+        {
+            var sign = Math.Sign(number);
+            var reverse = 0;
+            while (number > 0)
+            {
+                // Find the last digit
+                reverse = reverse * 10 + number % 10;
+
+                // Remove the last digit
+                number /= 10;
+            }
+
+            return reverse * sign;
+        }
+    }
+}
