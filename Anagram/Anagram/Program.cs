11434195db2f4a347aceb809cec68732893b50a9
Mohammad Sahebhonar <msahebhonar@yahoo.com>
2021-04-16 22:27:07 +0430

    Anagram
    One string is an anagram of another if it uses the same characters in the same number!

---
+++ Anagram/Anagram/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Is 'Anagram' an anagram of 'Nag A Ram!'? {Anagrams("Anagram", "Nag A Ram!")}");
+            Console.WriteLine($"Is 'School master' an anagram of 'The classroom'? {AnagramsLinq("Anagram", "Nag A Ram!")}");
+        }
+
+        public static bool Anagrams(string strA, string strB)
+        {
+            if (strA.Equals(strB))
+            {
+                return true;
+            }
+
+            var strACharDic = BuildCharDic(strA);
+            var strBCharDic = BuildCharDic(strB);
+
+            if (strACharDic.Keys.Count != strBCharDic.Keys.Count)
+            {
+                return false;
+            }
+
+            foreach (var character in strACharDic.Keys)
+            {
+                if (!strBCharDic.ContainsKey(character) ||
+                    strACharDic[character] != strBCharDic[character])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static Dictionary<char, int> BuildCharDic(string str)
+        {
+            var charDic = new Dictionary<char, int>();
+            var refinedStr = Regex.Replace(str, @"\W", "").ToLower();
+
+            foreach (var character in refinedStr)
+            {
+                if (!charDic.TryGetValue(character, out var i))
+                {
+                    charDic.Add(character, 1);
+                }
+                else
+                {
+                    charDic[character] = ++i;
+                }
+            }
+
+            return charDic;
+        }
+
+        public static bool AnagramsLinq(string strA, string strB)
+        {
+            return RefineString(strA)
+                .OrderBy(a => a)
+                .SequenceEqual(
+                    RefineString(strB)
+                        .OrderBy(b => b)
+                );
+        }
+        
+        private static string RefineString(string str)
+        {
+            return Regex.Replace(str, @"\W", "").ToLower();
+        }
+    }
+}
